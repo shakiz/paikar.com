84fb59a1c0df82d96e290fe46c6d4d829d1325c1
shakiz <activities335@gmail.com>
2020-12-02 00:55:11 +0600

    homeui part 1

--- app/src/main/java/com/client/paikarcom/activities/home/HomeActivity.java
+++ app/src/main/java/com/client/paikarcom/activities/home/HomeActivity.java
@@ -1,22 +1,77 @@
 package com.client.paikarcom.activities.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 
 import com.client.paikarcom.R;
+import com.client.paikarcom.adapters.SlidingImageAdapter;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeActivity extends AppCompatActivity {
+    private static ViewPager mPager;
+    private static TabLayout tabLayout;
+    private static int currentPage = 0;
+    private static int NUM_PAGES = 0;
+    private static final Integer[] IMAGES= {R.drawable.banner_image_1,R.drawable.banner_image_2,R.drawable.banner_image_3,R.drawable.banner_image_4,R.drawable.banner_image_5};
+    private ArrayList<Integer> ImagesArray = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.md_green_800));
+        setSupportActionBar(toolbar);
+
+        init();
+    }
+
+    private void init() {
+        for(int i=0;i<IMAGES.length;i++) {
+            ImagesArray.add(IMAGES[i]);
+        }
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        tabLayout = (TabLayout) findViewById(R.id.tabDots);
+        mPager.setAdapter(new SlidingImageAdapter(ImagesArray));
+        tabLayout.setupWithViewPager(mPager);
+
+        NUM_PAGES =IMAGES.length;
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == NUM_PAGES) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 3000, 3000);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
     }
 }
---
+++ app/src/main/java/com/client/paikarcom/adapters/SlidingImageAdapter.java
@@ -0,0 +1,56 @@
+package com.client.paikarcom.adapters;
+
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.viewpager.widget.PagerAdapter;
+import com.client.paikarcom.R;
+import java.util.ArrayList;
+
+public class SlidingImageAdapter extends PagerAdapter {
+    private ArrayList<Integer> IMAGES;
+
+    public SlidingImageAdapter(ArrayList<Integer> IMAGES) {
+        this.IMAGES=IMAGES;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return IMAGES.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+        View imageLayout = LayoutInflater.from(view.getContext()).inflate(R.layout.sliding_images_layout, view, false);
+
+        assert imageLayout != null;
+        final ImageView imageView = (ImageView) imageLayout
+                .findViewById(R.id.image);
+
+        imageView.setImageResource(IMAGES.get(position));
+        view.addView(imageLayout, 0);
+        return imageLayout;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+    }
+
+    @Override
+    public Parcelable saveState() {
+        return null;
+    }
+
+}
