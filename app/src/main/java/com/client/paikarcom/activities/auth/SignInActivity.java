5e52a095ae81102a198790c6d42712270b7ee162
shakiz <activities335@gmail.com>
2020-12-26 22:44:57 +0600

    test

--- app/src/main/java/com/client/paikarcom/activities/auth/SignInActivity.java
+++ app/src/main/java/com/client/paikarcom/activities/auth/SignInActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
