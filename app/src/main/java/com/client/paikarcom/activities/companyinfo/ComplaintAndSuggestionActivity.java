dcd68b1978fe14abe56f1ae9e34fb1228a2713f5
shakiz <activities335@gmail.com>
2020-12-30 00:35:38 +0600

    complain and suggestion

--- app/src/main/java/com/client/paikarcom/activities/companyinfo/ComplaintAndSuggestionActivity.java
+++ app/src/main/java/com/client/paikarcom/activities/companyinfo/ComplaintAndSuggestionActivity.java
@@ -1,16 +1,52 @@
 package com.client.paikarcom.activities.companyinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.client.paikarcom.R;
+import com.client.paikarcom.activities.home.HomeActivity;
+import com.client.paikarcom.databinding.ActivityComplaintAndSuggestionBinding;
 
 public class ComplaintAndSuggestionActivity extends AppCompatActivity {
+    private ActivityComplaintAndSuggestionBinding activityComplaintAndSuggestionBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_complaint_and_suggestion);
+        activityComplaintAndSuggestionBinding = DataBindingUtil.setContentView(this, R.layout.activity_complaint_and_suggestion);
+
+        //region init UI and perform UI interactions
+        initUI();
+        bindUiWithComponents();
+        //endregion
     }
+
+    //region init UI
+    private void initUI() {
+    }
+    //endregion
+
+    //region perform UI interactions
+    private void bindUiWithComponents() {
+        //region back arrow
+        activityComplaintAndSuggestionBinding.BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        //endregion
+    }
+    //endregion
+
+    //region activity components
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(ComplaintAndSuggestionActivity.this, HomeActivity.class));
+    }
+    //endregion
 }
